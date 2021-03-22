ca11b6e251bab24b9d02dba6ef3b076dfecd9e7c
Danil <58917020+Leonardo2526@users.noreply.github.com>
2021-03-22 14:35:04 +0300

    Add files via upload

---
+++ DS_RevitApp_WPF_FW4.8_RibbonTab/MyApplication.cs
@@ -0,0 +1,53 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DS.RevitApp.RibbonTab
+{
+    public class MyApplication : IExternalApplication
+    {
+        // class instance
+        internal static MyApplication thisApp = null;
+
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Create a custom ribbon tab
+            string tabName = "DS_Tab";
+            application.CreateRibbonTab(tabName);
+
+            var button1Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS_RevitApp_RVTtoNWC_FW4.8\DS_RevitApp_RVTtoNWC_FW4.8.dll");
+            var button2Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS_RevitApp_FamiliesUpdate_4.8\DS_RevitApp_FamiliesUpdate_4.8.dll");
+            //var button3Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS_RevitApp_FamiliesUpdate_4.8\DS_RevitApp_FamiliesUpdate_4.8.dll");
+
+
+            // Create two push buttons
+            //PushButtonData button1 = new PushButtonData("Button1", "Hello World", button1Path, "DS_RevitSpace.HelloWorld");
+            PushButtonData button1 = new PushButtonData("Button1", "DS_RVTtoNWC.v1.2", button1Path, "DS.RevitApp.RVTtoNWC.DS_MainClass");
+            PushButtonData button2 = new PushButtonData("Button2", "DS_FamiliesUpdate.v2", button2Path, "DS.RevitApp.FamiliesUpdate.EntryCommand");
+
+
+            // Create a ribbon panel
+            RibbonPanel m_projectPanel_1 = application.CreateRibbonPanel(tabName, "Panel_1");
+            //RibbonPanel m_projectPanel_2 = application.CreateRibbonPanel(tabName, "DS_Panel_2");
+
+            // Add the buttons to the panel
+            List<RibbonItem> projectButtons = new List<RibbonItem>();
+            projectButtons.AddRange(m_projectPanel_1.AddStackedItems(button1, button2));
+            //projectButtons.AddRange(m_projectPanel_2.AddStackedItems(button1, button2));
+
+
+            return Result.Succeeded;
+        }
+
+
+    }
+
+
+}
