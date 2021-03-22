d884ce4ba363cfff1ec55423691bc9bb64fb912d
Danil <leonardo147wot@gmail.com>
2021-03-22 15:02:15 +0300

    Moved

---
+++ RVTtoNWC_FW4.8/00_Archive/210218_DS_RVTtoNWC.cs
@@ -0,0 +1,391 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using DS_FilterForm;
+using DS_Forms;
+using DS_SystemTools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using View = Autodesk.Revit.DB.View;
+
+namespace DS_RVTtoNWC
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class DS_MainClass : IExternalCommand
+
+    {
+        //Get current date and time  
+        readonly string CurDate = DateTime.Now.ToString("yyMMdd");
+        readonly string CurDateTime = DateTime.Now.ToString("yyMMdd_HHmmss");
+
+        public int FileSize { get; set; }
+        public int FileDate { get; set; }
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+
+        List<string> FileFiltered = new List<string>();
+        List<string> EmptyModels = new List<string>();
+
+        List<string> DirNWClist = new List<string>();
+        List<string> NewDirNWClist = new List<string>();
+
+
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and documnet objects
+            UIApplication uiapp = commandData.Application;
+
+            uiapp.Application.FailuresProcessing += Application_FailuresProcessing;
+
+            try
+            {
+                DS_StartForms();
+
+                DS_Filter filter = new DS_Filter(uiapp)
+                {
+                    TopLevel = true,
+                    sourcePath = SourcePath,
+                    destinationPath = DestinationPath
+                };
+
+                filter.Show();
+            }
+
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+
+        public void DS_StartForms()
+        {
+            DS_Form newForm = new DS_Form
+            {
+                TopLevel = true
+            };
+
+            SourcePath = newForm.DS_OpenInputFolderDialogForm("Set folder for checking '*.rvt' files").ToString();
+            if (SourcePath == "")
+            {
+                return;
+            }
+            newForm.Close();
+
+            DestinationPath = newForm.DS_OpenOutputFolderDialogForm("Set output folder for '*.nwc' files").ToString();
+            if (DestinationPath == "")
+            {
+                return;
+            }
+
+            newForm.Close();
+
+            
+        }
+
+        public void DS_MainProgram(UIApplication uiapp)
+        //Intiating main process
+        {
+            DS_Tools dS_Tools = new DS_Tools
+            {
+                DS_LogName = CurDateTime + "_Log.txt",
+                DS_LogOutputPath = DestinationPath + "\\" + "Log" + "\\"
+            };
+            //Get all exist directories in DestinationPath
+            GetDirNamesNWC();
+
+            GetFiles(uiapp, SourcePath, dS_Tools);
+
+            DirIterate(uiapp, SourcePath, dS_Tools);
+
+            //Saved file names with pathes write to log
+            if (FileFiltered.Count != 0)
+            {
+                dS_Tools.DS_StreamWriter("NWC files have been created: ");
+                dS_Tools.DS_StreamWriter(dS_Tools.DS_ListToString(FileFiltered));
+                MessageBox.Show("Process has been completed successfully!");
+            }
+
+            else
+            {
+                dS_Tools.DS_StreamWriter("No NWC files have been created.");
+                MessageBox.Show("Process has been completed with errors. \n See log file.");
+            }           
+
+            //Saved file names with empty models
+            if (EmptyModels.Count != 0)
+            {
+                dS_Tools.DS_StreamWriter("\n Files with no geometry have been found: ");
+                dS_Tools.DS_StreamWriter(dS_Tools.DS_ListToString(EmptyModels));
+            }
+
+            //Saved file names with empty models
+            if (NewDirNWClist.Count != 0)
+            {
+                dS_Tools.DS_StreamWriter("\n New directories have been created: ");
+                dS_Tools.DS_StreamWriter(dS_Tools.DS_ListToString(NewDirNWClist));
+            }
+            dS_Tools.DS_FileExistMessage();
+        }
+
+        public void DirIterate(UIApplication uiapp, string CheckedPath, DS_Tools dS_Tools)
+        //Output files names list and it's direcories to Log
+        {
+
+            try
+            {
+                //Check folders
+                foreach (string dir in Directory.GetDirectories(CheckedPath))
+                {
+                    GetFiles(uiapp, dir, dS_Tools);
+                    DirIterate(uiapp, dir, dS_Tools);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void GetFiles(UIApplication uiapp, string dir, DS_Tools dS_Tools)
+        {
+            //Directory name write to log
+            //dS_Tools.DS_StreamWriter(dir);
+            //string dirName = new DirectoryInfo(dir).Name;
+
+            //Get files list from current directory
+            string ext = "rvt";
+            List<string> FileFullNames_Filtered = FileFilter(dir, ext);
+
+            foreach (string FileName in FileFullNames_Filtered)
+            {
+                string NWCFileName = Path.GetFileNameWithoutExtension(FileName);
+
+                NWC_Export(uiapp, FileName, NWCFileName);              
+            }
+        }
+         
+        List<string> FileFilter(string dir, string ext)
+
+        {
+            //Extensions list
+            var FileExt = new List<string> { ext };
+
+            List<string> FileFullNames = Directory.EnumerateFiles(dir, "*_*_*_*_*_*_*_*", SearchOption.TopDirectoryOnly).
+            Where(s => FileExt.Contains(Path.GetExtension(s).TrimStart((char)46).ToLowerInvariant())).ToList();
+
+            List<string> FileFullNames_Filtered = new List<string>();
+
+            FileFullNames_Filtered.AddRange(FileFullNames);
+
+            foreach (string fn in FileFullNames)
+            {
+                FileInfo f = new FileInfo(fn);
+                if (FileSize != 0 && f.Length < FileSize)
+                    FileFullNames_Filtered.Remove(fn);
+                if (FileDate != 0 && f.LastWriteTime < DateTime.Now.AddDays(-FileDate))
+                    FileFullNames_Filtered.Remove(fn);
+            }
+
+            return FileFullNames_Filtered;
+        }
+
+        public void NWC_Export(UIApplication uiapp, string FileName, string NWCFileName)
+        {
+            ModelPath mp = ModelPathUtils.ConvertUserVisiblePathToModelPath(FileName);
+
+            OpenOptions options1 = new OpenOptions
+            {
+                DetachFromCentralOption = DetachFromCentralOption.DetachAndDiscardWorksets
+            };
+
+            Document doc = uiapp.Application.OpenDocumentFile(mp, options1);
+            
+            DS_IsEmptyModel dS_IsEmptyModel = new DS_IsEmptyModel();
+            if (dS_IsEmptyModel.IsEmptyModel(doc))
+            {
+                EmptyModels.Add(FileName);
+                return;
+            }
+
+            string dirName = GetDirNWC(NWCFileName);
+
+            // Get a reference to each file in that directory.
+            string OutputPath = DestinationPath + "\\" + dirName + "\\";
+
+            NavisworksExportOptions nweo = new NavisworksExportOptions
+            {
+                ExportScope = NavisworksExportScope.View,
+                ViewId = NW_View(doc).Id
+            };
+            doc.Export(OutputPath, NWCFileName, nweo);
+
+            FileFiltered.Add(OutputPath + NWCFileName + ".nwc");
+            doc.Close(false); 
+        }
+
+
+        public View NW_View(Document doc)
+        {
+
+            FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
+            viewCollector.OfClass(typeof(View3D));
+
+            DS_Tools dS_ViewTools = new DS_Tools
+            {
+                DS_LogName = CurDateTime + "_ViewLog.txt"
+            };
+
+            //Navisworks view searching
+            foreach (Element viewElement in viewCollector)
+            {
+                View3D view = (View3D)viewElement;
+
+                if (view.Name.Contains("Navisworks"))
+                    return view;
+            }
+
+            //{3D} view searching
+            foreach (Element viewElement in viewCollector)
+            {
+                View3D view = (View3D)viewElement;
+
+                if (view.Name.Contains("{3D}"))
+                    return view;
+            }
+
+            dS_ViewTools.DS_FileExistMessage();
+
+            return null;
+        }
+
+        void Application_FailuresProcessing(object sender, Autodesk.Revit.DB.Events.FailuresProcessingEventArgs e)
+        {
+            FailuresAccessor fa = e.GetFailuresAccessor();
+            IList<FailureMessageAccessor> failList = new List<FailureMessageAccessor>();
+            failList = fa.GetFailureMessages(); // Inside event handler, get all warnings
+            foreach (FailureMessageAccessor failure in failList)
+            {
+
+                // check FailureDefinitionIds against ones that you want to dismiss, FailureDefinitionId failID = failure.GetFailureDefinitionId();
+                // prevent Revit from showing Unenclosed room warnings
+                FailureDefinitionId failID = failure.GetFailureDefinitionId();
+                if (failID == BuiltInFailures.WorksharingFailures.DuplicateNamesChanged)
+                {
+                    fa.DeleteWarning(failure);
+                }
+            }
+        }
+
+        public string GetDirNWC(string NWCFileName)
+        //Output files names list and it's direcories to Log
+        {
+            string DefaultOutputPath = DestinationPath + "\\" + NWCFileName + "\\";
+
+           
+            try
+            {
+                //Check folders
+                foreach (string dirname in DirNWClist)
+                {
+                    if (NWCFileName.Contains(dirname))
+                    return dirname;
+                }
+                Directory.CreateDirectory(DefaultOutputPath);
+                NewDirNWClist.Add(DefaultOutputPath);
+                return NWCFileName;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return "";
+        }
+
+        public void GetDirNamesNWC()
+        //Write directory names in DestinationPath to list
+        {
+            try
+            {
+                //Check folders
+                foreach (string dir in Directory.GetDirectories(DestinationPath))
+                {
+                    string dirName = new DirectoryInfo(dir).Name;
+                    DirNWClist.Add(dirName);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static List<string> FieldsGet(string line)
+        {
+            int i = 0;
+            int indS = 0;
+            int[] ind = new int[line.Length];
+
+            List<string> field = new List<string>();
+
+            //Through each symbol in file name iterating
+            foreach (char sign in line)
+            {
+                indS += 1;
+
+                if (sign.ToString() == "_")
+                {
+                    i += 1;
+
+                    //Record the index of findings
+                    ind[i] = indS;
+
+                    //LIst record of fields
+                    field.Add(line.Substring(ind[i - 1], ind[i] - ind[i - 1] - 1));
+                }
+            }
+            field.Add(line);
+            return field;
+        }
+
+    }
+     
+    public class DS_IsEmptyModel
+    //Check if active document has physical elements
+    {
+        public bool IsEmptyModel(Document doc)
+        {
+            // Use shortcut WhereElementIsNotElementType() to find wall instances only
+            FilteredElementCollector collector = new FilteredElementCollector(doc).WhereElementIsNotElementType();
+
+            foreach (Element e in collector)
+            {
+                if (IsPhysicalElement(e))
+                    return false;
+            }
+            return true;
+        }
+
+        bool IsPhysicalElement(Element e)
+        {
+            if (e.Category == null) return false;
+            if (e.ViewSpecific) return false;
+            if (e.LevelId == null) return false;
+
+            // exclude specific unwanted categories
+            if (((BuiltInCategory)e.Category.Id.IntegerValue) == BuiltInCategory.OST_HVAC_Zones) return false;
+
+            return e.Category.CategoryType == CategoryType.Model && e.Category.CanAddSubcategory;
+        }
+
+    }
+
+}
---
+++ RVTtoNWC_FW4.8/Filter.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace DS_FilterForm
+{
+    partial class DS_Filter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.ApplyFilter = new System.Windows.Forms.Button();
+            this.NoFilter = new System.Windows.Forms.Button();
+            this.flowLayoutPanel_Filters = new System.Windows.Forms.FlowLayoutPanel();
+            this.checkBox_File_size = new System.Windows.Forms.CheckBox();
+            this.textBox_File_size = new System.Windows.Forms.TextBox();
+            this.label_File_size = new System.Windows.Forms.Label();
+            this.checkBox_File_date = new System.Windows.Forms.CheckBox();
+            this.textBox_File_date = new System.Windows.Forms.TextBox();
+            this.label_File_date = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel_Filters.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel_Filters, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(450, 271);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.ApplyFilter);
+            this.flowLayoutPanel2.Controls.Add(this.NoFilter);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 211);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(444, 57);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // ApplyFilter
+            // 
+            this.ApplyFilter.Location = new System.Drawing.Point(3, 3);
+            this.ApplyFilter.Name = "ApplyFilter";
+            this.ApplyFilter.Size = new System.Drawing.Size(111, 42);
+            this.ApplyFilter.TabIndex = 0;
+            this.ApplyFilter.Text = "ApplyFilter\r\n";
+            this.ApplyFilter.UseVisualStyleBackColor = true;
+            this.ApplyFilter.Click += new System.EventHandler(this.ApplyFilter_Click);
+            // 
+            // NoFilter
+            // 
+            this.NoFilter.Location = new System.Drawing.Point(120, 3);
+            this.NoFilter.Name = "NoFilter";
+            this.NoFilter.Size = new System.Drawing.Size(141, 42);
+            this.NoFilter.TabIndex = 1;
+            this.NoFilter.Text = "Continue without filer";
+            this.NoFilter.UseVisualStyleBackColor = true;
+            this.NoFilter.Click += new System.EventHandler(this.NoFilter_Click_1);
+            // 
+            // flowLayoutPanel_Filters
+            // 
+            this.flowLayoutPanel_Filters.Controls.Add(this.checkBox_File_size);
+            this.flowLayoutPanel_Filters.Controls.Add(this.textBox_File_size);
+            this.flowLayoutPanel_Filters.Controls.Add(this.label_File_size);
+            this.flowLayoutPanel_Filters.Controls.Add(this.checkBox_File_date);
+            this.flowLayoutPanel_Filters.Controls.Add(this.textBox_File_date);
+            this.flowLayoutPanel_Filters.Controls.Add(this.label_File_date);
+            this.flowLayoutPanel_Filters.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel_Filters.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel_Filters.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel_Filters.Name = "flowLayoutPanel_Filters";
+            this.flowLayoutPanel_Filters.Size = new System.Drawing.Size(444, 202);
+            this.flowLayoutPanel_Filters.TabIndex = 3;
+            // 
+            // checkBox_File_size
+            // 
+            this.checkBox_File_size.AutoSize = true;
+            this.checkBox_File_size.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.checkBox_File_size.Location = new System.Drawing.Point(3, 3);
+            this.checkBox_File_size.Name = "checkBox_File_size";
+            this.checkBox_File_size.Size = new System.Drawing.Size(92, 24);
+            this.checkBox_File_size.TabIndex = 2;
+            this.checkBox_File_size.Text = "File size";
+            this.checkBox_File_size.UseVisualStyleBackColor = true;
+            this.checkBox_File_size.CheckedChanged += new System.EventHandler(this.checkBox_File_size_CheckedChanged);
+            // 
+            // textBox_File_size
+            // 
+            this.textBox_File_size.Location = new System.Drawing.Point(3, 33);
+            this.textBox_File_size.Name = "textBox_File_size";
+            this.textBox_File_size.Size = new System.Drawing.Size(100, 26);
+            this.textBox_File_size.TabIndex = 3;
+            this.textBox_File_size.Tag = "";
+            this.textBox_File_size.Visible = false;
+            this.textBox_File_size.TextChanged += new System.EventHandler(this.textBox_File_size_TextChanged);
+            // 
+            // label_File_size
+            // 
+            this.label_File_size.AutoSize = true;
+            this.label_File_size.Location = new System.Drawing.Point(3, 62);
+            this.label_File_size.Name = "label_File_size";
+            this.label_File_size.Size = new System.Drawing.Size(256, 20);
+            this.label_File_size.TabIndex = 4;
+            this.label_File_size.Text = "Enter the smallest size of a file, MB";
+            this.label_File_size.Visible = false;
+            this.label_File_size.Click += new System.EventHandler(this.label_File_size_Click);
+            // 
+            // checkBox_File_date
+            // 
+            this.checkBox_File_date.AutoSize = true;
+            this.checkBox_File_date.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.checkBox_File_date.Location = new System.Drawing.Point(3, 85);
+            this.checkBox_File_date.Name = "checkBox_File_date";
+            this.checkBox_File_date.Size = new System.Drawing.Size(96, 24);
+            this.checkBox_File_date.TabIndex = 5;
+            this.checkBox_File_date.Text = "File date";
+            this.checkBox_File_date.UseVisualStyleBackColor = true;
+            this.checkBox_File_date.CheckedChanged += new System.EventHandler(this.checkBox_File_date_CheckedChanged);
+            // 
+            // textBox_File_date
+            // 
+            this.textBox_File_date.Location = new System.Drawing.Point(3, 115);
+            this.textBox_File_date.Name = "textBox_File_date";
+            this.textBox_File_date.Size = new System.Drawing.Size(100, 26);
+            this.textBox_File_date.TabIndex = 6;
+            this.textBox_File_date.Tag = "";
+            this.textBox_File_date.Visible = false;
+            this.textBox_File_date.TextChanged += new System.EventHandler(this.textBox_File_date_TextChanged);
+            // 
+            // label_File_date
+            // 
+            this.label_File_date.AutoSize = true;
+            this.label_File_date.Location = new System.Drawing.Point(3, 144);
+            this.label_File_date.Name = "label_File_date";
+            this.label_File_date.Size = new System.Drawing.Size(394, 20);
+            this.label_File_date.TabIndex = 7;
+            this.label_File_date.Text = "Enter the last file write date (in days from current date).";
+            this.label_File_date.Visible = false;
+            // 
+            // DS_Filter
+            // 
+            this.ClientSize = new System.Drawing.Size(450, 271);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Name = "DS_Filter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Filters applying";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel_Filters.ResumeLayout(false);
+            this.flowLayoutPanel_Filters.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
