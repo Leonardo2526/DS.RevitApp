069ef0430f198ffa8c51ca5aa9b70c56a53a06dd
Danil <leonardo147wot@gmail.com>
2021-03-23 11:20:21 +0300

    Project created

---
+++ AddProjectParameters/DS_RevitApp_Template.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AddProjectParameters
+{
+    [Transaction(TransactionMode.Manual)]
+    public class EntryCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData revit,
+         ref string message, ElementSet elements)
+        {
+            try
+            {
+                UserControl1 userControl = new UserControl1();
+                userControl.Show();
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+        }
+    }
+
+}
