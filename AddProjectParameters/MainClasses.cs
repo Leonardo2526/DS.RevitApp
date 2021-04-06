37abd22a7b0aa572fd6ed6520f4ab884bcf16814
Danil <leonardo147wot@gmail.com>
2021-04-06 17:18:47 +0300

    Parameters loading to projects added

--- AddProjectParameters/MainClasses.cs
+++ AddProjectParameters/MainClasses.cs
@@ -26,27 +26,80 @@ namespace AddProjectParameters
             this.App = app;
         }
 
-
-        public void ExecuteLoadProcess()
+        public void IterateThroughtFiles()
         {
-            Document doc = App.ActiveUIDocument.Document;
+            int i = 0;
+            try
+            {
+                //Through each file iterating
+                foreach (string file in StartForm.FilesPathes)
+                {
+                    i++;
+                    Document doc = App.Application.OpenDocumentFile(file);
+                    CategorySet cats = SetCatergoty(doc);
 
+                    foreach (string parameter in StartForm.SelectesParameters)
+                        AddParameterToProject(doc, cats, false, BuiltInParameterGroup.PG_DATA, parameter);
+
+                    doc.Close(true);
+                    //Logs write
+                    //LoadReport.Add(i + ". File path: " + doc.PathName + "\n");
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        
+           
+
+            /*
+            DS_Tools dS_Tools = new DS_Tools
+            {
+                DS_LogName = CurDateTime + "_Log.txt",
+                DS_LogOutputPath = DialogForms.SourcePath + "\\" + "Log" + "\\"
+            };
+            WriteLogToFile(dS_Tools);
+            */
+
+            TaskDialog.Show("Revit", "Process completed successfully!");
+
+            MyApplication.thisApp.m_MyForm.Close();
+
+        }
+
+        CategorySet SetCatergoty(Document doc)
+        {
             //Category set forming
             Category wall = doc.Settings.Categories.get_Item(BuiltInCategory.OST_Walls);
             Category door = doc.Settings.Categories.get_Item(BuiltInCategory.OST_Doors);
-            CategorySet cats1 = App.Application.Create.NewCategorySet();
-            cats1.Insert(wall);
-            cats1.Insert(door);
+            CategorySet cats = App.Application.Create.NewCategorySet();
+            cats.Insert(wall);
+            cats.Insert(door);
 
-
-            CreateProjectParameter(App.Application, doc, "NewParameter1", ParameterType.Text, true, cats1, BuiltInParameterGroup.PG_DATA, false);
-           
-
-            MyApplication.thisApp.m_MyForm.Close();
+            return cats;
+            //CreateProjectParameter(App.Application, doc, "NewParameter1", ParameterType.Text, true, cats1, BuiltInParameterGroup.PG_DATA, false);        
              
         }
 
-        public void CreateProjectParameter(RvtApplication app, Document doc, string name, ParameterType type, bool visible, CategorySet cats, BuiltInParameterGroup group, bool inst)
+
+        void AddParameterToProject(Document doc, CategorySet cats, bool inst, BuiltInParameterGroup group, string parameterName)
+        {
+            App.Application.SharedParametersFilename = StartForm.SPFPath;
+
+            ExternalDefinition def = App.Application.OpenSharedParameterFile().Groups.
+                get_Item(SelectParameters.SelectedGroupName).Definitions.get_Item(parameterName) as ExternalDefinition;
+
+            Autodesk.Revit.DB.Binding binding = App.Application.Create.NewTypeBinding(cats);
+            //if (inst) binding = App.Application.Create.NewInstanceBinding(cats);
+
+            TransactionCommit(doc, def, binding, group);
+        }
+
+
+        public void CreateProjectParameter(RvtApplication app, Document doc, string name, 
+            ParameterType type, bool visible, CategorySet cats, BuiltInParameterGroup group, bool inst)
         {
             //InternalDefinition def = new InternalDefinition();
             //Definition def = new Definition();
@@ -77,17 +130,16 @@ namespace AddProjectParameters
 
        
 
-
-
         private void TransactionCommit(Document doc, ExternalDefinition def, Autodesk.Revit.DB.Binding binding , BuiltInParameterGroup group)
         {
-            using (Transaction transNew = new Transaction(doc, "RealLoading"))
+            using (Transaction transNew = new Transaction(doc, "Create project parameter"))
             {
                 try
                 {
+                    transNew.Start();
                     // The name of the transaction was given as an argument
-                    if (transNew.Start("Create project parameter") != TransactionStatus.Started) 
-                        return ;
+                    //if (transNew.Start("Create project parameter") != TransactionStatus.Started) 
+                        //return ;
 
                     BindingMap map = doc.ParameterBindings;
                     map.Insert(def, binding, group);
@@ -101,7 +153,6 @@ namespace AddProjectParameters
                 }
 
                 transNew.Commit();
-                //doc.Close(false);
             }
         }
     }
