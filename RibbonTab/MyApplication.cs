d7940f8500df2d81c762e844b33827e42898da1a
Danil <leonardo147wot@gmail.com>
2021-04-13 10:53:55 +0300

    AddSharedParameters

--- RibbonTab/MyApplication.cs
+++ RibbonTab/MyApplication.cs
@@ -25,6 +25,8 @@ namespace DS.RevitApp.RibbonTab
             var button1Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.RVTtoNWC\DS.RVTtoNWC.dll");
             var button2Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.FamiliesUpdate\DS.FamiliesUpdate.dll");
             var button3Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.AddProjectParameters\DS.AddProjectParameters.dll");
+            var button4Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.AddSharedParameters\DS.AddSharedParameters.dll");
+
 
 
             // Create two push buttons
@@ -32,17 +34,19 @@ namespace DS.RevitApp.RibbonTab
             PushButtonData button1 = new PushButtonData("Button1", "RVTtoNWC", button1Path, "DS.RevitApp.RVTtoNWC.DS_MainClass");
             PushButtonData button2 = new PushButtonData("Button2", "FamiliesUpdate", button2Path, "DS.RevitApp.FamiliesUpdate.EntryCommand");
             PushButtonData button3 = new PushButtonData("Button3", "AddProjectParameters", button3Path, "AddProjectParameters.EntryCommand");
+            PushButtonData button4 = new PushButtonData("Button4", "AddSharedParameters", button4Path, "AddSharedParameters.EntryCommand");
+
 
             button1.ToolTip = "Export files from *.rvt to *.nwc. \nVer.2";
 
             // Create a ribbon panel
             RibbonPanel m_projectPanel_1 = application.CreateRibbonPanel(tabName, "Tools");
-            //RibbonPanel m_projectPanel_2 = application.CreateRibbonPanel(tabName, "DS_Panel_2");
+            RibbonPanel m_projectPanel_2 = application.CreateRibbonPanel(tabName, "Parameters");
 
             // Add the buttons to the panel
             List<RibbonItem> projectButtons = new List<RibbonItem>();
-            projectButtons.AddRange(m_projectPanel_1.AddStackedItems(button1, button2, button3));
-            //projectButtons.AddRange(m_projectPanel_2.AddStackedItems(button1, button2));
+            projectButtons.AddRange(m_projectPanel_1.AddStackedItems(button1, button2));
+            projectButtons.AddRange(m_projectPanel_2.AddStackedItems(button3, button4));
 
 
             return Result.Succeeded;
