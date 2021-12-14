bc0a6fc95b7409d192c88a2915895df995c44900
Leonardo2526 <leonardo147wot@gmail.com>
2021-12-14 10:24:47 +0300

    Disallowjoin deleted

--- RibbonTab/MyApplication.cs
+++ RibbonTab/MyApplication.cs
@@ -25,7 +25,6 @@ namespace DS.RevitApp.RibbonTab
             var button2Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.FamiliesUpdate\DS.FamiliesUpdate.dll");
             var button3Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.AddSharedParameters\DS.AddSharedParameters.dll");
             var button4Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.AddProjectParameters\DS.AddProjectParameters.dll");
-            var button5Path = Environment.ExpandEnvironmentVariables(@"%AppData%\Autodesk\Revit\Addins\2020\DS.DisallowjoinStructure\DS.DisallowjoinStructure.dll");
             
 
 
@@ -34,14 +33,12 @@ namespace DS.RevitApp.RibbonTab
             PushButtonData button2 = new PushButtonData("Button2", "FamiliesUpdate", button2Path, "DS.RevitApp.FamiliesUpdate.EntryCommand");
             PushButtonData button3 = new PushButtonData("Button3", "AddSharedParameters", button3Path, "AddSharedParameters.EntryCommand");
             PushButtonData button4 = new PushButtonData("Button4", "AddProjectParameters", button4Path, "AddProjectParameters.EntryCommand");
-            PushButtonData button5 = new PushButtonData("Button5", "DisallowjoinStructure", button5Path, "DisallowjoinStructure.EntryCommand");
            
             // Create tool tips 
             button1.ToolTip = "Export files from *.rvt to *.nwc.\nv2.0";
             button2.ToolTip = "Update families in *.rvt files.\nv1.2";
             button3.ToolTip = "Add new parameters to shared parameters file.\nv1.0";
             button4.ToolTip = "Add new parameters from shared parameters file to *.rvt projects.\nv1.0";
-            button5.ToolTip = "Disallow beams joining in current document.\nv1.0";
 
 
             // Create a ribbon panel
@@ -52,7 +49,7 @@ namespace DS.RevitApp.RibbonTab
             // Add the buttons to the panels
             List<RibbonItem> projectButtons = new List<RibbonItem>();
             projectButtons.AddRange(m_projectPanel_1.AddStackedItems(button1, button2));
-            projectButtons.AddRange(m_projectPanel_2.AddStackedItems(button3, button4, button5));
+            projectButtons.AddRange(m_projectPanel_2.AddStackedItems(button3, button4));
 
             return Result.Succeeded;
         }
