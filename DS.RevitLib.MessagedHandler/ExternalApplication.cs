0daa5cb737ab984bd51ff5e426ab0c01518aabaf
Данил Сараев <leonardo147wot@gmail.com>
2023-01-20 16:52:49 +0300

    MessagesHandler app added

---
+++ DS.RevitLib.MessagedHandler/ExternalApplication.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.RevitLib.MessageHandler
+{
+    public class ExternalApplication : IExternalApplication
+    {
+        /// Implement this method to subscribe to event.
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                // Register event. 
+                application.ControlledApplication.FailuresProcessing += ControlledApplication_FailuresProcessing;
+                //TaskDialog.Show("DS message", "Message handler1 acivated!");
+
+            }
+            catch (Exception)
+            {
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // remove the event.
+            application.ControlledApplication.FailuresProcessing -= ControlledApplication_FailuresProcessing;
+            return Result.Succeeded;
+        }
+
+        private void ControlledApplication_FailuresProcessing(object sender, FailuresProcessingEventArgs e)
+        {
+            FailuresAccessor fa = e.GetFailuresAccessor();
+            var failList = fa.GetFailureMessages();
+
+            var messagesHandler = new MessagesHandler(e);
+        }
+
+    }
+
+}
---
+++ DS.RevitLib.MessagedHandler/MessagesHandler.cs
@@ -0,0 +1,82 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.RevitLib.MessageHandler
+{
+
+    internal class MessagesHandler
+    {
+        private readonly FailuresAccessor _failuresAccessor;
+        private readonly string _transactionName;
+        private readonly FailuresProcessingEventArgs _eventArgs;
+
+        /// <summary>
+        /// Handle transaction messages
+        /// </summary>
+        public MessagesHandler(FailuresProcessingEventArgs failuresProcessingEventArgs)
+        {
+            this._eventArgs = failuresProcessingEventArgs;
+            _failuresAccessor = failuresProcessingEventArgs.GetFailuresAccessor();
+            _transactionName = _failuresAccessor.GetTransactionName();
+
+            List<FailureMessageAccessor> errorMessages =
+                (List<FailureMessageAccessor>)_failuresAccessor.GetFailureMessages(FailureSeverity.Error);
+            HandleErrors(errorMessages);
+
+            List<FailureMessageAccessor> warningMessages =
+                (List<FailureMessageAccessor>)_failuresAccessor.GetFailureMessages(FailureSeverity.Warning);
+            HandleWarnings(warningMessages);
+        }
+
+        public bool ShowFails { get; private set; } = false;
+
+        public void HandleErrors(List<FailureMessageAccessor> errorMessages)
+        {
+            if (errorMessages != null && errorMessages.Any())
+            {
+                //TaskDialog.Show("errorMessages: ", errorMessages.Count.ToString());
+
+                foreach (var m in errorMessages)
+                {
+                    var type = m.GetCurrentResolutionType();
+                    m.SetCurrentResolutionType(type);
+
+                    if (!ShowFails)
+                    {
+                        //Set option for failure handling
+                        FailureHandlingOptions failureHandlingOptions = _failuresAccessor.GetFailureHandlingOptions();
+                        failureHandlingOptions.SetClearAfterRollback(true);
+                        _failuresAccessor.SetFailureHandlingOptions(failureHandlingOptions);
+
+                        _eventArgs.SetProcessingResult(FailureProcessingResult.ProceedWithRollBack);
+                    }
+                    else
+                    {
+                        _eventArgs.SetProcessingResult(FailureProcessingResult.ProceedWithRollBack);
+                    }
+                    _failuresAccessor.SetTransactionName(_transactionName + "-RolledBack");
+                    _failuresAccessor.DeleteWarning(m);
+                }
+            }
+        }
+
+        public void HandleWarnings(List<FailureMessageAccessor> warningMessages)
+        {
+            if (warningMessages != null && warningMessages.Any())
+            {
+                //TaskDialog.Show("warningMessages: ", warningMessages.Count.ToString());
+                foreach (var m in warningMessages)
+                {
+                    _failuresAccessor.SetTransactionName(_transactionName + "-Warning");
+                    _failuresAccessor.DeleteWarning(m);
+                }
+            }
+        }
+    }
+}
