f2643023468efe40b7b326f7dbd5db55472a5868
Danil <leonardo147wot@gmail.com>
2024-01-29 21:15:18 +0300

    redundant classes removed

--- DS.MEPCurveTraversability/DS.MEPCurveTraversability.Interactors/ValidatorFactories/WallIntersectionValidatorFactory.cs
+++ DS.MEPCurveTraversability/DS.MEPCurveTraversability.Interactors/ValidatorFactories/WallIntersectionValidatorFactory.cs
@@ -1,15 +1,20 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using DS.ClassLib.VarUtils;
-using DS.ClassLib.VarUtils.Collisons;
-using DS.MEPCurveTraversability.Interactors.Settings;
-using OLMP.RevitAPI.Core.Extensions;
 using OLMP.RevitAPI.Tools;
+using OLMP.RevitAPI.Tools.Collisions;
 using OLMP.RevitAPI.Tools.Creation.Transactions;
+using OLMP.RevitAPI.Tools.Extensions;
+using OLMP.RevitAPI.Tools.Intersections;
+using OLMP.RevitAPI.Tools.Openings;
+using OLMP.RevitAPI.Tools.OpeningsCreator;
+using Rhino;
+using Rhino.Geometry;
 using Serilog;
-using Serilog.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace DS.MEPCurveTraversability.Interactors
 {
@@ -54,16 +59,80 @@ namespace DS.MEPCurveTraversability.Interactors
             var solidIntersectionFactory = new SolidElementIntersectionFactory(_doc, _localFilter)
             { Logger = Logger, TransactionFactory = TransactionFactory };
 
-            return new WallsChecker(
+            //build filters
+            Func<Wall, Vector3d, bool> directionFilter = null;
+            if (_wallIntersectionSettings.NormalAngleLimit < RhinoMath.ToRadians(90))
+            {
+                directionFilter = (wall, dir) =>
+                wall.IsTraversable(dir,
+                _doc,
+                _allLoadedLinks,
+                _wallIntersectionSettings.NormalAngleLimit);
+            }
+
+            Func<Solid, Wall, bool> openingFilter = null;
+            if (_wallIntersectionSettings.CheckOpenings)
+            {
+                openingFilter = (wall, solid) =>
+                IsOpeningTraversable(
+                    _doc,
+                    _allLoadedLinks,
+                    wall,
+                    solid,
+                    _wallIntersectionSettings,
+                    TransactionFactory,
+                    Logger);
+            }
+
+            return new WallIntersectionValidator(
               _uiDoc,
               _allLoadedLinks,
-              solidIntersectionFactory,
-              _wallIntersectionSettings)
+              solidIntersectionFactory)
             {
+                DirectionFilter = directionFilter,
+                OpeningFilter = openingFilter,
                 Logger = Logger,
                 TransactionFactory = TransactionFactory,
                 WindowMessenger = WindowMessenger
             };
         }
+
+        private bool IsOpeningTraversable(
+            Document activeDoc,
+            IEnumerable<RevitLinkInstance> links,
+              Solid solid,
+              Wall wall,
+              IWallIntersectionSettings wallIntersectionSettings, 
+              ITransactionFactory transactionFactory, 
+              ILogger logger)
+        {
+            var profileCreator = new RectangleSolidProfileCreator(activeDoc, links)
+            {
+                Offset = wallIntersectionSettings.OpeningOffset,
+                TransactionFactory = transactionFactory,
+                Logger = logger
+            };
+            var wBuilder = new WallOpeningProfileValidator<Solid>(activeDoc, links, profileCreator)
+            {
+                InsertsOffset = wallIntersectionSettings.InsertsOffset,
+                WallOffset = wallIntersectionSettings.WallOffset,
+                JointsOffset = wallIntersectionSettings.JointsOffset,
+                Logger = logger,
+                TransactionFactory = transactionFactory
+            };
+
+            var mArg = (wall as Wall, solid);
+
+            var b = wBuilder.IsValid(mArg);
+            if (!b)
+            {
+                var sb = new StringBuilder();
+                wBuilder.ValidationResults.ToList().ForEach(r => sb.Append(r.ErrorMessage));
+                logger?.Information(sb.ToString());
+            }
+
+            return b;
+        }
+
     }
 }
